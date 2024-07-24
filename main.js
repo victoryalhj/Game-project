3a2eb6716a6828ad81956f659c5ce51a48eb8dc3
victoryalhj <victoryalhj@gmail.com>
2024-07-24 20:52:20 +0100

    set the randomnum

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let currentMoleTile;
+let currPlantTile;
 
 
 window.onload = function() {
@@ -13,10 +14,47 @@ function setGame() {
     tile.id = i.toString();
     document.getElementById("board").appendChild(tile)
   }
+  setInterval(setMole, 2000); //2000 milliseconds = 2 seconds
+  setInterval(setPlant, 3000);
 }
 
+function getRandomTile() {
+  //math.random > (0-1)*9 = 0 -9 > round down to (0-8) integers
+  let num = Math.floor(Math.random()*9);
+  return num.toString();
+}
+
+
 function setMole() {
+  //reset mole  
+  if(currentMoleTile) {
+    currentMoleTile.innerHTML = "";
+  }
 
   let mole = document.createElement("img");
-  mole.src = ""
+  mole.src = "./img/Mole.png";
+
+  let num = getRandomTile();
+  //Set the plant and mole to avoid the same tile 
+  if (currPlantTile && currPlantTile.id == num) {
+    return;
+  }
+  currentMoleTile = document.getElementById(num);
+  currentMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+  if (currPlantTile) {
+    currPlantTile.innerHTML = "";
+  }
+
+  let plant = document.createElement("img");
+  plant.src = "./img/Plant.png";
+
+  let num = getRandomTile();
+  if (currentMoleTile && currentMoleTile.id == num) {
+    return;
+  }
+  currPlantTile = document.getElementById(num);
+  currPlantTile.appendChild(plant);
 }
